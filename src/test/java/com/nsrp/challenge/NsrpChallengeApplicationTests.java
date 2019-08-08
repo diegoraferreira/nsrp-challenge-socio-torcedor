8fc9206927076f99eb3168a75b0f028f0b349be2
diegoraferreira <diegoraferreira@gmail.com>
2019-08-08 20:24:12 -0300

    Separando o escopo de teste pra rodar em h2

--- src/test/java/com/nsrp/challenge/NsrpChallengeApplicationTests.java
+++ src/test/java/com/nsrp/challenge/NsrpChallengeApplicationTests.java
@@ -3,13 +3,15 @@ package com.nsrp.challenge;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@SpringBootTest
-//@RunWith(SpringRunner.class)
-//public class NsrpChallengeApplicationTests {
-//
-//    @Test
-//    public void contextLoads() {
-//    }
-//}
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class NsrpChallengeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
